33bfac05eafaf0f1eb70a56f6c6d714a823dc296
Jeerawat Keawsawat <gukaihero@gmail.com>
2019-09-01 12:00:47 +0700

    Add Page Project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCwBva0HFJS1BEJeyNCWzcScms-SkSG-Ok",
+    authDomain: "ionic-firebase-app-4c2ba.firebaseapp.com",
+    databaseURL: "https://ionic-firebase-app-4c2ba.firebaseio.com",
+    projectId: "ionic-firebase-app-4c2ba",
+    storageBucket: "",
+    messagingSenderId: "433088783370",
+    appId: "1:433088783370:web:44fa3a86c142e955"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCwBva0HFJS1BEJeyNCWzcScms-SkSG-Ok",
+    authDomain: "ionic-firebase-app-4c2ba.firebaseapp.com",
+    databaseURL: "https://ionic-firebase-app-4c2ba.firebaseio.com",
+    projectId: "ionic-firebase-app-4c2ba",
+    storageBucket: "",
+    messagingSenderId: "433088783370",
+    appId: "1:433088783370:web:44fa3a86c142e955"
+  }
 };
 
 /*
